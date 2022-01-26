47bb74c8c6b34bd01e941cea25e74247aae86fc7
Graza <code@graza.io>
2022-01-26 16:48:22 +0000

    feat: products

--- freshservice/product.go
+++ freshservice/product.go
@@ -1,6 +1,150 @@
 package freshservice
 
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+const (
+	productsUrl  = "products"
+	productIdUrl = "products/%d"
+)
+
 // ProductService API Docs: https://api.freshservice.com/#products
 type ProductService struct {
-    client *Client
+	client *Client
+}
+
+// Products contains Collection an array of Product
+type Products struct {
+	Collection []Product `json:"products"`
+}
+
+// productWrapper contains Details of a Product
+type productWrapper struct {
+	Details Product `json:"product"`
+}
+
+// Product represents a FreshService Product
+type Product struct {
+	ID                 int       `json:"id"`
+	Name               string    `json:"name"`
+	Description        string    `json:"description"`
+	AssetTypeID        int       `json:"asset_type_id"`
+	Manufacturer       string    `json:"manufacturer"`
+	Status             string    `json:"status"`
+	ModeOfProcurement  string    `json:"mode_of_procurement"`
+	DepreciationTypeID int       `json:"depreciation_type_id"`
+	DescriptionText    string    `json:"description_text"`
+	CreatedAt          time.Time `json:"created_at"`
+	UpdatedAt          time.Time `json:"updated_at"`
+}
+
+// CreateProductModel is a data struct for creating a new Product
+type CreateProductModel struct {
+	Name               string `json:"name"`
+	Description        string `json:"description"`
+	AssetTypeID        int    `json:"asset_type_id"`
+	Manufacturer       string `json:"manufacturer"`
+	Status             string `json:"status"`
+	ModeOfProcurement  string `json:"mode_of_procurement"`
+	DepreciationTypeID int    `json:"depreciation_type_id"`
+	DescriptionText    string `json:"description_text"`
+}
+
+// UpdateProductModel is a data struct for updating a Product
+type UpdateProductModel struct {
+	Name               string `json:"name"`
+	Description        string `json:"description"`
+	AssetTypeID        int    `json:"asset_type_id"`
+	Manufacturer       string `json:"manufacturer"`
+	Status             string `json:"status"`
+	ModeOfProcurement  string `json:"mode_of_procurement"`
+	DepreciationTypeID int    `json:"depreciation_type_id"`
+	DescriptionText    string `json:"description_text"`
+}
+
+// ListProductsOptions represents filters/pagination for Products
+type ListProductsOptions struct {
+	ListOptions
+}
+
+// GetProduct will return a Product by id
+func (s *ProductService) GetProduct(id int) (*Product, *http.Response, error) {
+	req, err := s.client.NewRequest(http.MethodGet, fmt.Sprintf(productIdUrl, id), nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	p := new(productWrapper)
+	res, err := s.client.SendRequest(req, &p)
+	if b, s := isSuccessful(res); !b {
+		return nil, res, fmt.Errorf("%s: %v", s, err)
+	}
+
+	return &p.Details, res, nil
+}
+
+// ListProducts will return paginated/filtered Products using ListProductsOptions
+func (s *ProductService) ListProducts(opt *ListProductsOptions) (*Products, *http.Response, error) {
+	req, err := s.client.NewRequest(http.MethodGet, productsUrl, opt)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	ps := new(Products)
+	res, err := s.client.SendRequest(req, &ps)
+	if b, s := isSuccessful(res); !b {
+		return nil, res, fmt.Errorf("%s: %v", s, err)
+	}
+
+	return ps, res, nil
+}
+
+// CreateProduct will create and return a new Product based on CreateProductModel
+func (s *ProductService) CreateProduct(newProduct *CreateProductModel) (*Product, *http.Response, error) {
+	req, err := s.client.NewRequest(http.MethodPost, productsUrl, newProduct)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	p := new(productWrapper)
+	res, err := s.client.SendRequest(req, &p)
+	if b, s := isSuccessful(res); !b {
+		return nil, res, fmt.Errorf("%s: %v", s, err)
+	}
+
+	return &p.Details, res, nil
+}
+
+// UpdateProduct will update and return a Product matching id based UpdateProductModel
+func (s *ProductService) UpdateProduct(id int, product *UpdateLocationModel) (*Product, *http.Response, error) {
+	req, err := s.client.NewRequest(http.MethodPut, fmt.Sprintf(productIdUrl, id), product)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	p := new(productWrapper)
+	res, err := s.client.SendRequest(req, &p)
+	if b, s := isSuccessful(res); !b {
+		return nil, res, fmt.Errorf("%s: %v", s, err)
+	}
+
+	return &p.Details, res, nil
+}
+
+// DeleteProduct will completely remove a Product from FreshService matching id
+func (s *ProductService) DeleteProduct(id int) (bool, *http.Response, error) {
+	req, err := s.client.NewRequest(http.MethodDelete, fmt.Sprintf(productIdUrl, id), nil)
+	if err != nil {
+		return false, nil, err
+	}
+
+	res, err := s.client.SendRequest(req, nil)
+	if b, s := isSuccessful(res); !b {
+		return false, res, fmt.Errorf("%s: %v", s, err)
+	}
+
+	return true, res, nil
 }
